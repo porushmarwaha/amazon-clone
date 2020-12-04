2b0ddb2f9494dd9f9ac4512ac36f0c48b8844579
Rahul Marwaha <Rahul@cloudthing.com>
2020-12-04 18:09:17 +0530

    The initial phase of project

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBxjpthnaFaDM6CfIejQPdj1d7VUlG8ToY",
+  authDomain: "challenge-b2912.firebaseapp.com",
+  projectId: "challenge-b2912",
+  storageBucket: "challenge-b2912.appspot.com",
+  messagingSenderId: "608598914553",
+  appId: "1:608598914553:web:1bab347737cb044cc73928",
+  measurementId: "G-SX3GJ83C4H",
+};
